c13bf5d149a5724ce24ed883ab3f84ad4d1a1e27
miglange <motamiglange@gmail.com>
2019-12-12 08:54:21 +0100

    first commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Router, Route } from 'react-router';
+import ReactDOM from 'react-dom';
+
+
+import App from './App';
+
+ReactDOM.render(<App/>, document.querySelector('#root'));
